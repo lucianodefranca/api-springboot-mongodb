23b521f129d4fad9790c998b7eadc205cbe2b311
Luciano de França <lucianodefranca@gmail.com>
2021-12-25 19:14:15 -0300

    Obtendo usuario por ID, excecao personalizada

--- src/main/java/com/luciano/apimongo/services/UserService.java
+++ src/main/java/com/luciano/apimongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.luciano.apimongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luciano.apimongo.domain.User;
 import com.luciano.apimongo.repositories.UserRepository;
+import com.luciano.apimongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 
 }
