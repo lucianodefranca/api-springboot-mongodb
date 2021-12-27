48d1e459f595b3f553e8b00adeb5ee1ce8eb1dd6
Luciano de França <lucianodefranca@gmail.com>
2021-12-27 18:13:05 -0300

    Endpoint para retornar posts de um usuario

--- src/main/java/com/luciano/apimongo/resources/UserResource.java
+++ src/main/java/com/luciano/apimongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luciano.apimongo.domain.Post;
 import com.luciano.apimongo.domain.User;
 import com.luciano.apimongo.dto.UserDTO;
 import com.luciano.apimongo.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 
 }
