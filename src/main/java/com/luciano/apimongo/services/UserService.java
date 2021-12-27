62179e9c6063b681e6672f05045510e6cf2559ef
Luciano de França <lucianodefranca@gmail.com>
2021-12-26 22:15:42 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/luciano/apimongo/services/UserService.java
+++ src/main/java/com/luciano/apimongo/services/UserService.java
@@ -13,28 +13,43 @@ import com.luciano.apimongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		repo.findById(obj.getId());
+		if (obj.getId() != null) {
+			User newObj = obj;
+			updateData(newObj, obj);
+			return repo.save(newObj);
+		}
+		return null;
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDto(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
