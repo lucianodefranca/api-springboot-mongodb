e5757f3cbf9a487f414949c95820c3438db2d18b
Luciano de França <lucianodefranca@gmail.com>
2021-12-27 18:36:06 -0300

    Adicionando comentarios aos Posts

--- src/main/java/com/luciano/apimongo/domain/Post.java
+++ src/main/java/com/luciano/apimongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.luciano.apimongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.luciano.apimongo.dto.AuthorDTO;
+import com.luciano.apimongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -70,6 +75,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
