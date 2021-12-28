cd85b8a5034798783b610fc9ff548d29f77026d3
Luciano de França <lucianodefranca@gmail.com>
2021-12-27 21:44:19 -0300

    Consulta com varios criterios

--- src/main/java/com/luciano/apimongo/services/PostService.java
+++ src/main/java/com/luciano/apimongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.luciano.apimongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,10 @@ public class PostService {
 	
 	public List<Post> findByTitle (String text) {
 		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date min, Date max) {
+		max = new Date(max.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, min, max);
 	}
 }
